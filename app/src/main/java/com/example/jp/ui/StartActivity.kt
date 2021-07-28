f24620b01d11c593471e113b654d78caf1be6fe4
DESKTOP-393D1HA\11014 <1101403985@qq.com>
2021-07-29 00:03:27 +0800

    启动图

--- app/src/main/java/com/example/jp/ui/StartActivity.kt
+++ app/src/main/java/com/example/jp/ui/StartActivity.kt
@@ -2,30 +2,40 @@ package com.example.jp.ui
 
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.android.arouter.launcher.ARouter
 import com.eftimoff.androipathview.PathView
+import com.example.common.arouter.RouterUrl
 import com.example.jp.R
 
 
 class StartActivity : AppCompatActivity() {
-    private val pathView = findViewById<View>(R.id.pathView) as PathView
+    private var pathView:PathView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        fun jump() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        pathView = findViewById<View>(R.id.pathView) as PathView
 
-        }
         pathView?.pathAnimator
-            .delay(100)
-            .duration(5000)
-            .listenerEnd(PathView.AnimatorBuilder.ListenerEnd() {
+            ?.delay(100)
+            ?.duration(5000)
+            ?.listenerEnd(PathView.AnimatorBuilder.ListenerEnd() {
 
-                @Override fun onAnimationEnd() {
+                @Override
+                fun onAnimationEnd() {
                     // 监听动画完成之后的跳转实行
                     jump();
                 }
-            }).start();
+            })?.start();
 
+    }
+
+    private fun jump(){
+        ARouter.getInstance().build(RouterUrl.APP.Tab).navigation()
     }
 
 }
